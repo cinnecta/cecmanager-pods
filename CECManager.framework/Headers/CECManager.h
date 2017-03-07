2e4c52e329bbfba52bbc618c82656cad10520b8f
LucasFreitas <lucas_but@hotmail.com>
2017-03-07 10:48:54 -0300

    first commit

---
+++ CECManager.framework/Headers/CECManager.h
@@ -0,0 +1,19 @@
+//
+//  CECManager.h
+//  CECManager
+//
+//  Created by Lucas Freitas de Oliveira on 29/12/16.
+//  Copyright © 2016 Cinnecta. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CECManager.
+FOUNDATION_EXPORT double CECManagerVersionNumber;
+
+//! Project version string for ClientTracker.
+FOUNDATION_EXPORT const unsigned char CECManagerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CECManager/PublicHeader.h>
+
+
